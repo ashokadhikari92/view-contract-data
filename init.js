d5e64be69eb887de28f48fe8316f7c1095977821
iTs mE ) <ashok.adhikari92@gmail.com>
2015-07-03 15:59:26 +0545

    minor change in init.js file

--- init.js
+++ init.js
@@ -58,17 +58,15 @@ function loadContractData(id){
 	else{
 
 			var latlon = result[id].latlon;
-			
+
 			var commaPos = latlon.indexOf(',');
 			var coordinatesLat = parseFloat(latlon.substring(0, commaPos));
 			var coordinatesLong = parseFloat(latlon.substring(commaPos + 1, latlon.length));
-			console.log(coordinatesLat,coordinatesLong);
-
-		 var mapProp = {
-			    center:new google.maps.LatLng(28.2639,84.4333),
-			    zoom:6,
-			    mapTypeId:google.maps.MapTypeId.ROADMAP
-			  };
+             var mapProp = {
+                    center:new google.maps.LatLng(28.2639,84.4333),
+                    zoom:6,
+                    mapTypeId:google.maps.MapTypeId.ROADMAP
+                  };
 			  var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
 			  var myLatlng = new google.maps.LatLng(coordinatesLat,coordinatesLong);
 			  var marker = new google.maps.Marker({
@@ -76,10 +74,29 @@ function loadContractData(id){
 			      map: map
 			  });
 
-			  google.maps.event.addDomListener(window, 'load');
+			  google.maps.event.addDomListener(window, 'load',initialize);
 
 
 	}
 				
 }
 
+function initialize(){
+
+    var latlon = result[id].latlon;
+
+    var commaPos = latlon.indexOf(',');
+    var coordinatesLat = parseFloat(latlon.substring(0, commaPos));
+    var coordinatesLong = parseFloat(latlon.substring(commaPos + 1, latlon.length));
+    var mapProp = {
+        center:new google.maps.LatLng(28.2639,84.4333),
+        zoom:6,
+        mapTypeId:google.maps.MapTypeId.ROADMAP
+    };
+    var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+    var myLatlng = new google.maps.LatLng(coordinatesLat,coordinatesLong);
+    var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map
+    });
+}
